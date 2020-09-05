0250522920aca3bf873ec6fe7cffeb12d403d6e4
yuta shimogishi <yuta.shimogishi@tryattain.com>
2020-09-05 10:20:00 +0900

    VSCode Plugin追加

--- vscodeUtil/install-vscode-plugins.sh
+++ vscodeUtil/install-vscode-plugins.sh
@@ -8,6 +8,22 @@
 # vscode marketplaceのpluginページのURLのitemName名を列挙する
 pkglist=(
 # 例 mhutchie.git-graph
+vscode-icons-team.vscode-icons # アイコンをリッチにする
+mhutchie.git-graph # gitグラフをみやすくする
+eamodio.gitlens # git blameなどいろいろできる
+esbenp.prettier-vscode # コードを自動整形する
+donjayamanne.githistory # git historyがみやすくなる
+CoenraadS.bracket-pair-colorizer # 対応するカッコを色付け
+wayou.vscode-todo-highlight # TODOハイライト
+MS-CEINTL.vscode-language-pack-ja # 日本語化
+ionutvmi.path-autocomplete # Path入力を補完する
+mechatroner.rainbow-csv # CSVを色付けする
+ryu1kn.partial-diff # 部分的にDiffをみる
+GitHub.vscode-pull-request-github # GithubPRを作る
+IBM.output-colorizer # Outputに色をつける
+rogalmic.bash-debug # Bash作成をサポート
+shardulm94.trailing-spaces # 行末の空白をハイライト
+stevencl.addDocComments # jsdoc追加
 )
 
 for i in ${pkglist[@]}; do
